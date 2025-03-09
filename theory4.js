88f07aba31a7a942ba1871cdc16a44c6e1981696
CoolShivas <shivas2710cool00@gmail.com>
2025-03-09 13:08:30 +0530

    Prerequisites and Installation of Node.js

---
+++ theory4.js
@@ -0,0 +1,89 @@
+/***
+ * 
+ * * * * * Prerequisites to Learn Node.js :-
+ * 
+ * 
+        HTML 
+        CSS
+        JavaScript :-
+                    • Basic & Intermediate
+                    • ES6 & Above
+
+//////---------------------------------------------------------------------------------------------------------
+
+ * 
+ * 
+ * 
+ * Node.JS Installation :-
+
+ * 
+ * ------Setting up Development Environment :-
+ * 
+ * 
+        Download Node.js
+        Visual Studio Code :-
+                            •Prettier
+                            •Turn on Format on Save
+
+
+
+//////---------------------------------------------------------------------------------------------------------
+ * 
+ * 
+ * 
+ *  Installing Node.js and npm (FNM) :-
+ * 
+ * 
+        • You can download Node.js from its official website. We will be using FNM in this course.
+        
+        • FNM (Fast Node Manager) is a command-line utility that enables developers to easily manage and switch between different versions of Node.js on their machines.
+
+        • We are using v23.2.0 in this course, but all the content in this course will be valid for upcoming versions too.
+ 
+        • If you don’t have winget on Windows, then you can also use chocolatey, or scoop to install FNM. Details are 
+        mentioned here.
+
+        • Of course, you need to install chocolatey or scoop first.
+
+
+
+ //////---------------------------------------------------------------------------------------------------------
+
+
+ * 
+ * 
+ *  REPL (Read-Eval Print Loop) :-
+ * 
+ * 
+ * 
+ * -----Introduction to REPL (Read-Eval-Print Loop) :-
+ * 
+    • It stands for Read-Eval-Print Loop or Read, Evaluate, Print and Loop. It’s an interactive programming environment that allows you to execute JavaScript code one statement at a time.
+
+    • You can open your terminal and use node command.
+
+    • You can use it to test simple JavaScript code similar to browser devtools.
+
+    • Ctrl + C to exit out of it.
+
+
+-------Each part of the acronym highlights a specific step in how the REPL works :-
+ 
+    • Read: The REPL reads the user’s input (a single line or multiple lines of code) and parses it into a data structure
+    that the JavaScript engine can understand.
+
+    • Eval (Evaluate): The parsed input is evaluated (executed) by the JavaScript engine. If the input is a valid expression, the REPL computes the result.
+
+    • Print: The result of the evaluated expression is printed back to the console so the user can see the output.
+ 
+    • Loop: The process then loops back, waiting for the next input, and continues until the user exits
+
+
+
+//////---------------------------------------------------------------------------------------------------------
+ 
+ * 
+ * 
+ * 
+ * 
+ */
