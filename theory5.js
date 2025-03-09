796e7c2d8c5f661cb7a61c372c3145c2788d95f9
CoolShivas <shivas2710cool00@gmail.com>
2025-03-09 13:13:30 +0530

    Node.js Basics

---
+++ theory5.js
@@ -0,0 +1,57 @@
+/***
+ * 
+ * 
+ * * * *  Node.js Basics :-
+ * 
+ * 
+    Understanding the Node.js Module System
+    Creating and Organizing Modules
+    Common Node.js Built-in Modules
+    Streams and Buffers
+    Introduction to package.json
+    ES Modules in Node.js.
+
+ //////---------------------------------------------------------------------------------------------------------
+
+
+ * 
+ * 
+ * * * Creating Your First "Hello World" Application :--
+ * 
+ * 
+ * ----What is CLI ? :-
+ * 
+ * 
+ *      
+    • CLI (Command Line Interface) is a way to execute predefined JavaScript files or run commands for tasks like installing packages, running scripts, or starting applications.
+ 
+
+ * ----When to Use CLI ? :-
+ * 
+ * 
+ *  
+    • Building Applications: Execute your scripts to run applications or servers.
+    • Automation: Use npm scripts for repetitive tasks (e.g., building or testing code).
+    • Large-Scale Projects: Execute full files for consistent and reusable logic
+
+
+//////---------------------------------------------------------------------------------------------------------
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
